71a427f7630906876111c83ea90a3a0802436b74
Kinfe123 <kinfishtech@gmail.com>
2025-11-25 11:18:45 +0300

    chore: import js

--- src/cli.ts
+++ src/cli.ts
@@ -8,10 +8,8 @@ import { Command } from 'commander';
 import { findAuthConfig } from './config.js';
 import { startStudio } from './studio.js';
 import { detectDatabaseWithDialect } from './utils/database-detection.js';
-import { possibleConfigFiles } from './utils';
+import { possibleConfigFiles } from './utils.js';
 async function findAuthConfigPath(): Promise<string | null> {
-  
-
   let currentDir = process.cwd();
   const maxDepth = 10;
   let depth = 0;
