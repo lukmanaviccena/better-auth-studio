1bd971263c9baa58bdcfe72e18687a34409ecd8c
Kinfe123 <kinfishtech@gmail.com>
2025-12-16 23:44:44 +0300

    chore: clean up

--- docs/src/app/changelog/page.tsx
+++ docs/src/app/changelog/page.tsx
@@ -1,9 +1,6 @@
 import PixelLayout from "@/components/PixelLayout";
 import PixelCard from "@/components/PixelCard";
 
-const IconWrapper = ({ children, className = "" }: { children: React.ReactNode; className?: string }) => (
-  <span className={`inline-flex items-center ${className}`}>{children}</span>
-);
 
 const ZapIcon = () => (
   <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4 mr-2 text-white/70">
